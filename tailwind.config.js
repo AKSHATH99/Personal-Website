b8df87f9510f8e4f6592182ca98767ee0815ce8c
AKSHATH P <akshathpkk@gmail.com>
2024-01-28 20:17:36 +0530

    changed config for custom media queries

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,18 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        'sm': '640px',
+        'md': '768px',
+        'lg': '1024px',
+        'xl': '1280px',
+        '2xl': '1440px',
+        '800': '800px', 
+        '320': '320px'
+        // Set a custom breakpoint for 800px width
+      },
+    },
   },
   plugins: [],
 }
