af96bdf462c00af4af0c36a469911909f0c14e81
Akshath P <akshathpkk@gmail.com>
2024-12-16 22:21:34 +0530

    changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,21 @@
+const colors = require('tailwindcss/colors')
+
 /** @type {import('tailwindcss').Config} */
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,js}"],
+  mode:"jit",
   theme: {
     extend: {
+      animation: {
+        marquee: 'marquee 25s linear infinite',
+      },
+      keyframes: {
+        marquee: {
+          '0%': { transform: 'translateX(0%)' },
+          '100%': { transform: 'translateX(-100%)' },
+        },
+      },
       screens: {
         'sm': '640px',
         'md': '768px',
@@ -13,6 +25,12 @@ module.exports = {
         '800': '800px', 
         '320': '320px'
         // Set a custom breakpoint for 800px width
+      },
+      fontFamily: {
+        roboto: ['Roboto', 'sans-serif'],
+        edu: ['"Edu AU VIC WA NT Pre"', 'sans-serif'],
+        poppins: ['Poppins', 'sans-serif'],
+
       },
     },
   },
